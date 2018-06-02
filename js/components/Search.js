ded90b223a6ef5b3b056a8ed8b29a01c9a12272f
Anna Gajewska <gajewska.anna@o2.pl>
2018-06-02 21:40:43 +0200

    Change indentations

--- js/components/Search.js
+++ js/components/Search.js
@@ -23,18 +23,18 @@ Search = React.createClass({
 	render: function() {
 		var styles = {
 			fontSize: '1.5em',
-            width: '90%',
-            maxWidth: '350px'
+			width: '90%',
+			maxWidth: '350px'
 		};
 
-        return (
-            <input
-                type="text"
+		return (
+			<input 
+				type="text"
                 onChange={this.handleChange}
                 placeholder="Tutaj wpisz wyszukiwaną frazę"
                 style={styles}
                 value={this.state.searchTerm}
-            />
-        );
+			/>
+		)
 	}
 });
